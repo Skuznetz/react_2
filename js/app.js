eb85e63cde82780554904000f9fc539d5561385b
Skuznetz <s.kuznetzov@mail.ru>
2017-11-15 21:22:38 +0300

    создадим шаблон

--- js/app.js
+++ js/app.js
@@ -15,9 +15,18 @@ text: 'Бесплатно. Скачать. Лучший сайт - http://localh
 
 var News = React.createClass({
 render: function() {
+var data = this.props.data;
+var newsTemplate = data.map(function(item, index) {
+return (
+<div key={index}>
+<p className="news__author">{item.author}:</p>
+<p className="news__text">{item.text}</p>
+</div>
+)
+})
 return (
 <div className="news">
-К сожалению, новостей нет.
+{newsTemplate}
 </div>
 );
 }
