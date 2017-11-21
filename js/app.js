ca55dddce6262ada3959de6e68b0a9e4f5ef7a2f
Skuznetz <s.kuznetzov@mail.ru>
2017-11-21 10:03:26 +0300

    Убираем компонент Comments

--- js/app.js
+++ js/app.js
@@ -37,22 +37,13 @@ return (
 );
 }
 });
-var Comments = React.createClass({
-render: function() {
-return (
-<div className="comments">
-Нет новостей - комментировать нечего
-</div>
-);
-}
-});
+
 var App = React.createClass({
 render: function() {
 return (
 <div className="app">
 Всем привет, я компонент App! Я умею отображать новости.
 <News data={my_news} /> {/*добавили свойство data */}
-<Comments />
 </div>
 );
 }
