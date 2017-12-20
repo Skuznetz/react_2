97af655ff11b778ecb5ca991d9fe9f64f85afa3f
kuznetz <s.kuznetzov@mail.ru>
2017-12-20 19:56:12 +0300

    переменная visible

--- js/app.js
+++ js/app.js
@@ -34,7 +34,8 @@ var Article = React.createClass({
     render: function() {
         var author = this.props.data.author,
             text = this.props.data.text,
-            bigText = this.props.data.bigText;
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
         return ( <
             div className = 'article' >
             <
