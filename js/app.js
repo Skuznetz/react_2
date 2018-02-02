da8405f6dc6c7543ff07ad7b9125da29737444c9
kuznetz <s.kuznetzov@mail.ru>
2018-02-02 21:59:20 +0300

    добавим <testInput /> в <App />

--- js/app.js
+++ js/app.js
@@ -93,7 +93,7 @@ var App = React.createClass({
     render: function(){
         return (
             <div className="app">
-            
+              <TestInput />
                  <h1>Новости</h1>
             <News data={my_news} />
             </div>);
