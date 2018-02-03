af61a0ead7ea457d95845ba21a8abd8ad1f55946
kuznetz <s.kuznetzov@mail.ru>
2018-02-03 10:28:45 +0300

    обработаем код

--- js/app.js
+++ js/app.js
@@ -1,120 +1,129 @@
 var TestInput = React.createClass({
-    onChangeHandler: function(e){
-      this.setState({myValue: e.target.value})
-    },
-    getInitialState: function(){
-        return {myValue: ''};
-    },
-    onBtnClickHandler: function(e){
-      alert(this.state.myValue);
-    },
-    render: function(){
-  return (<div>
-    <input className='test-input' value={this.state.myValue} 
-    onChange={this.onChangeHandler} placeholder="введите значение" />
-    <button onClick={this.onBtnClickHandler}>Показать alert</button>
-    </div>);}
-    
-  });
-var my_news = [{
-        author: 'Саша Пушкин',
-        text: 'В четверг в четыре четверти часа',
-        bigText: '4 черненьких чумазеньких чертёнка'
-    },
-    {
-        author: 'Просто Петя',
-        text: 'Считаю, что $ должен стоить 35 рублей!',
-        bigText: 'ну может быть -75'
+            onChangeHandler: function(e) {
+                this.setState({ myValue: e.target.value })
+            },
+            getInitialState: function() {
+                return { myValue: '' };
+            },
+            onBtnClickHandler: function(e) {
+                alert(this.state.myValue);
+            },
+            render: function() {
+                return ( < div >
+                    <
+                    input className = 'test-input'
+                    value = { this.state.myValue } onChange = { this.onChangeHandler } placeholder = "введите значение" / >
+                    <
+                    button onClick = { this.onBtnClickHandler } > Показать alert < /button> <
+                    /div>);}
 
-    },
-    {
-        author: 'Гость',
-        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
-        bigText: 'Ну не очень бесплатно'
-    },
-    {   author: 'Вася',
-        text: 'Московское время',
-        bigText: 'Кому сколько -5-00'
-    }
-];
-var Article = React.createClass({
-    propsTypes: {
-        data: React.PropTypes.shape({
-            author: React.PropTypes.string.isRequired,
-            text: React.PropTypes.string.isRequired,
-            bigText: React.PropTypes.string.isRequired
-        })
-    },
-    getInitialState: function(){
-        return {
-            visible: false
-        };
-},
-    readmoreClick: function(e){
-        e.preventDefault();
-        this.setState({visible: true});
-    },
-    render: function(){
-    var author = this.props.data.author;
-    var text = this.props.data.text;
-    var bigText = this.props.data.bigText;
-    var visible = this.state.visible;
-        return (
-            <div className="article">
-            <p className="news_author">{author}: </p>
-            <p className="news_text">{text}</p>
-            {/* для ссылки readmore: не показывай ссылку, если visible === true */}
-<a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none': '')}>Подробнее</a
->
-{/* для большо текста: не показывай текст, если visible === false */}
-<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
-            </div>)
-    }
-});
-var News = React.createClass({
-    propTypes: {
-        data: React.PropTypes.array.isRequired
-    },
-    getInitialState: function(){
-        return {counter: 0}
-    },
-    
-    
-    render: function(){
-        var data = this.props.data;
-        var newsTamplate;
-        if (data.length > 0){
-        newsTamplate = data.map(function(item,index){
-            return (
-                <div key={index}>
-                <Article data={item} />
-                
-                </div>)})
-        } else {
-            newsTamplate = <p> К сожалению,новостей нет.</p>}
-        
-                return (
-            <div className="news">
-            {newsTamplate}
-            <strong className={'news_count ' + (data.length > 0 ? '' : 'none')} >Всего новостей: {data.length}</strong>
-            </div>);
-    }
+                });
+            var my_news = [{
+                    author: 'Саша Пушкин',
+                    text: 'В четверг в четыре четверти часа',
+                    bigText: '4 черненьких чумазеньких чертёнка'
+                },
+                {
+                    author: 'Просто Петя',
+                    text: 'Считаю, что $ должен стоить 35 рублей!',
+                    bigText: 'ну может быть -75'
 
-});
+                },
+                {
+                    author: 'Гость',
+                    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+                    bigText: 'Ну не очень бесплатно'
+                },
+                {
+                    author: 'Вася',
+                    text: 'Московское время',
+                    bigText: 'Кому сколько -5-00'
+                }
+            ];
+            var Article = React.createClass({
+                        propsTypes: {
+                            data: React.PropTypes.shape({
+                                author: React.PropTypes.string.isRequired,
+                                text: React.PropTypes.string.isRequired,
+                                bigText: React.PropTypes.string.isRequired
+                            })
+                        },
+                        getInitialState: function() {
+                            return {
+                                visible: false
+                            };
+                        },
+                        readmoreClick: function(e) {
+                            e.preventDefault();
+                            this.setState({ visible: true });
+                        },
+                        render: function() {
+                            var author = this.props.data.author;
+                            var text = this.props.data.text;
+                            var bigText = this.props.data.bigText;
+                            var visible = this.state.visible;
+                            return ( <
+                                div className = "article" >
+                                <
+                                p className = "news_author" > { author }: < /p> <
+                                p className = "news_text" > { text } < /p> { /* для ссылки readmore: не показывай ссылку, если visible === true */ } <
+                                a href = "#"
+                                onClick = { this.readmoreClick } className = { 'news__readmore ' + (visible ? 'none' : '') } > Подробнее < /a >
+                                { /* для большо текста: не показывай текст, если visible === false */ } <
+                                p className = { 'news__big-text ' + (visible ? '' : 'none') } > { bigText } < /p> <
+                                /div>)
+                            }
+                        });
+                    var News = React.createClass({
+                                propTypes: {
+                                    data: React.PropTypes.array.isRequired
+                                },
+                                getInitialState: function() {
+                                    return { counter: 0 }
+                                },
 
-var App = React.createClass({
-    render: function(){
-        return (
-            <div className="app">
-              <TestInput />
-                 <h1>Новости</h1>
-            <News data={my_news} />
-            </div>);
-    }
-});   
 
-const opp = document.getElementById('root');
+                                render: function() {
+                                        var data = this.props.data;
+                                        var newsTamplate;
+                                        if (data.length > 0) {
+                                            newsTamplate = data.map(function(item, index) {
+                                                        return ( <
+                                                            div key = { index } >
+                                                            <
+                                                            Article data = { item }
+                                                            />
 
-ReactDOM.render( <
-    App /> ,
-    opp);
+                                                            <
+                                                            /div>)})
+                                                        }
+                                                        else {
+                                                            newsTamplate = < p > К сожалению, новостей нет. < /p>}
+
+                                                            return ( <
+                                                                div className = "news" > { newsTamplate } <
+                                                                strong className = { 'news_count ' + (data.length > 0 ? '' : 'none') } > Всего новостей: { data.length } < /strong> <
+                                                                /div>);
+                                                            }
+
+                                                        });
+
+                                                    var App = React.createClass({
+                                                                render: function() {
+                                                                    return ( <
+                                                                        div className = "app" >
+                                                                        <
+                                                                        TestInput / >
+                                                                        <
+                                                                        h1 > Новости < /h1> <
+                                                                        News data = { my_news }
+                                                                        /> <
+                                                                        /div>);
+                                                                    }
+                                                                });
+
+                                                            const opp = document.getElementById('root');
+
+                                                            ReactDOM.render( <
+                                                                App / > ,
+                                                                opp);
