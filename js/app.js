4f43d3fa134dfcde4ce70f4206205e31c6a5db89
kuznetz <s.kuznetzov@mail.ru>
2018-02-07 12:31:53 +0300

    пишем класс

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var Add = React.createClass({
                     <
                     input type='text' className = 'add-author'
                     defaultValue='' placeholder = "введите Ваше Имя" ref='author' />
-                    <textarea ></textarea>
+                    <textarea className='add-text'></textarea>
 
                     <
                     button className='add_btn' onClick = { this.onBtnClickHandler } ref='alert_button' > Показать alert < /button> </form>);}
