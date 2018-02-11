0a6c0a096b1b59ff85d23b0720e9a2afa2646826
kuznetz <s.kuznetzov@mail.ru>
2018-02-11 09:08:38 +0300

    СОЗДАДИМ переменные

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@ var Add = React.createClass({
              },            
              onBtnClickHandler: function(e) {
                 e.preventDefault();
+                var author=ReactDOM.findDOMNode(this.refs.author).value;
+                var text=ReactDOM.findDOMNode(this.refs.text).value;
+                alert(author + '\n'+ text);
             },
             onBtnClickRuleClick: function(e){
                 this.setState({btnIsDisabled: !this.state.btnIsDisabled});
