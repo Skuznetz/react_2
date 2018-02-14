a023e10eb45b46d815b13b414b4049fb477a5815
kuznetz <s.kuznetzov@mail.ru>
2018-02-14 21:11:16 +0300

    пишем обработчик onTextChange

--- js/app.js
+++ js/app.js
@@ -17,6 +17,18 @@ var Add = React.createClass({
             })
         }
     },
+    onTextChange: function(e){
+        if (e.target.value.trim().length > 0){
+            this.setState({
+                textIsEmpty: false
+            
+            } else {
+
+                this.setState({
+                    textIsEmpty: true
+                })
+            }
+            },
              componentDidMount(){
                 ReactDOM.findDOMNode(this.refs.author).focus();
              },            
